543ac1888c6727849bcb53ca617b0fdd14834936
JasonEl1 <1eliseijas@hdsb.ca>
2025-09-08 21:56:21 -0400

    speed up program execution

--- spellingBeeSolver.py
+++ spellingBeeSolver.py
@@ -1,5 +1,5 @@
 print("------")
-print("v.1.1")
+print("v.1.2")
 print("------")
 
 #read english words list from .txt file
@@ -15,24 +15,16 @@ letters = []
 for _ in range(6):
     letters.append(input())
 
-print("Please wait...\n")
-
+# add words to result list if contain necessary letters and center letter
 result = []
-
-#remove all items that include letters not in the puzzle
 for word in data:
     if len(word)>3:
         good = True
         for char in word:
             if letters.count(char) == 0 and char is not center:
                 good = False
-        if(good):
+        if(good and word.count(center)>0):
             result.append(word)
-
-#remove all items that don't include the center letter
-for index in result:
-    if index.count(center) == 0:
-        result.remove(index)
 
 print("Possible words:")
 print(*result, sep=", ")
