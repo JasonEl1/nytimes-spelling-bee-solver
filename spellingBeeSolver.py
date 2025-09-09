1b555ea4325215d874d90013ae636098969e6178
JasonEl1 <1eliseijas@hdsb.ca>
2025-09-08 21:52:35 -0400

    remove unecessary variables and decrease number of loops through data

--- spellingBeeSolver.py
+++ spellingBeeSolver.py
@@ -1,51 +1,41 @@
-print("v.1.0")
+print("------")
+print("v.1.1")
 print("------")
 
 #maximum word length. Can be modified
 maxLength = 7
 
 #read english words list from .txt file
-my_file = open("words.txt", "r")
-data = my_file.readlines()
-
-#remove newline characters
-counter = 0
-for i in data:
-    data[counter] = i.replace("\n", "")
-    counter += 1
+data = []
+file = open("words.txt", "r")
+for line in file:
+    data.append(line.strip("\n"))
 
 #get input letters
-letters = []
 center = input("Center letter : ")
-print("Enter the outer 6 letters : ")
-for x in range(6):
+print("Enter the outer 6 letters separated by 'enter': ")
+letters = []
+for _ in range(6):
     letters.append(input())
-    
-print("Please wait...")
-print("")
+
+print("Please wait...\n")
 
 result = []
 
 #remove all items that include letters not in the puzzle
-counter = 0
-good = True
 for word in data:
-    good = True
-    if len(word)<4:
-        good = False
-    for x in range(len(word)):
-        if letters.count(word[x]) == 0 and word[x] is not center:
-            good = False
-    if good :
-        result.append(word)
-    counter+=1
-    
-#remove all items that don't include the center letter
-final = []
-counter = 0
-for index in result:
-    if result[counter].count(center) > 0:
-        final.append(index)
-    counter+=1
+    if len(word)>3:
+        good = True
+        for char in word:
+            if letters.count(char) == 0 and char is not center:
+                good = False
+        if(good):
+            result.append(word)
 
-print(final)
+#remove all items that don't include the center letter
+for index in result:
+    if index.count(center) == 0:
+        result.remove(index)
+
+print("Possible words:")
+print(*result, sep=", ")
